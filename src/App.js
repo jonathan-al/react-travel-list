d1416f365fc14989cf3ed8eed42c1c1244095e3b
Jonathan Alcantara <jonathan.alcantara@improving.com>
2023-12-19 17:56:42 -0600

    Add Stats in footer

--- src/App.js
+++ src/App.js
@@ -99,12 +99,26 @@ const PackingList = ({
   )
 }
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    )
+  }
+
+  const numItems = items.length
+  const numPacked = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPacked / numItems) * 100)
+
   return (
     <footer className="stats">
       <em>
-        🧳 You have X items on your list, and you alreade packed X
-        (X%)
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list,
+          and you already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   )
@@ -140,7 +154,7 @@ const App = () => {
         onDeleteItems={handleDeleteItem}
         onPackItem={handlePackItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   )
 }
