d64d9fcd4cbebfd42556af1df8ee5e8efadf53b1
Jonathan Alcantara <jonathan.alcantara@improving.com>
2023-12-22 11:03:51 -0600

    Implement sorting

--- src/App.js
+++ src/App.js
@@ -83,10 +83,24 @@ const PackingList = ({
   onDeleteItems: handleDeleteItem,
   onPackItem: handlePackItem,
 }) => {
+  const sort = {
+    input: (items) => items,
+    description: (items) =>
+      items
+        .slice()
+        .sort((a, b) => a.description.localeCompare(b.description)),
+    packed: (items) =>
+      items.slice().sort((a, b) => a.packed - b.packed),
+  }
+
+  const defaultSort = Object.keys(sort)[0]
+  const [sortBy, setSortBy] = useState(defaultSort)
+  const sortedItems = sort[sortBy](items)
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -95,6 +109,17 @@ const PackingList = ({
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   )
 }
